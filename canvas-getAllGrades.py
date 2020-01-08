e255a720069cb83e6f1f98bdfd8fea29e751acf5
burturt <31748545+burturt@users.noreply.github.com>
2020-01-07 21:14:52 -0800

    Re-re-add config for the last time (I hope!)

--- canvas-getAllGrades.py
+++ canvas-getAllGrades.py
@@ -1,81 +1,134 @@
+#!/bin/python3
 # canvas-getAllGrades.py
 # Enjoy!
 
 import re
 import requests
 import sys
-from time import sleep
+from configparser import ConfigParser
 
 LINE = "---------------------------"
-
+configFileExists = False
 
 if len(sys.argv) != 1 and len(sys.argv) != 3 and len(sys.argv) != 4:
-    print("Oh no! Invalid arguments!")
-    print("Usage: python3 <script path> <CANVAS_URL> <CANVAS_API_TOKEN> [FILENAME_TO_SAVE_TO.txt]\n")
-    print("(You can also ditch the arguments for guided instructions)\n")
+    print("Invalid # of arguments")
+    print("Usage: python3 <script path> <CANVAS_URL> <CANVAS_API_TOKEN> [FILENAME_TO_SAVE_TO.txt]")
+    print("OR\npython3 <script path>")
     exit(1)
-else: 
-    print("\n" + LINE*2)
+else:
+    print("\n" + LINE * 2)
     print("Hello! Welcome to getAllGrades.py!\n")
-    print("This script utilizes your canvas API token to print a \nlist of your courses and grades in a human-readable format.")
-    print(LINE*2)
+    print(
+        "This script utilizes your canvas API token to print a \nlist of your courses and grades in a human-readable "
+        "format.")
+    print(LINE * 2)
 
 if (len(sys.argv) == 3) or (len(sys.argv) == 4):
     authtoken = sys.argv[2]
     instructure_domain = sys.argv[1]
+    createConfig = False
     if (len(sys.argv) == 4):
         arg3 = sys.argv[3]
         writeToFile = True
 else:
-    print("Try using arguments instead! python3 " + sys.argv[0] + " -h for more info.")
-    while True:
-        instructure_domain = input("\nPlease type the base URL for your canvas instance:\n(Leave empty for https://smuhsd.instructure.com/)\nURL: ")
-
-        if instructure_domain == "":
-            instructure_domain = "https://smuhsd.instructure.com/";
-            break
-
-        valURL = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
-        if valURL.match(instructure_domain):
-            print("Making a request to the url to verify it exists...")
-            try:
-                requests.get(instructure_domain)
-                break
-            except:
-                print("!!! Invalid URL. Please try again. !!!")
+    try:
+        config = ConfigParser()
+        config.read('config.ini')
+        authtoken = config.get('canvas_auth', 'access_token')
+        instructure_domain = config.get('canvas_auth', 'instance')
+    except:
+        print("No valid config file found")
+    else:
+        createConfig = False
+        configFileExists = True
+        print("Valid config file (config.ini) found, using to populate options")
+        try:
+            arg3 = config.get('canvas_auth', 'output_file')
+        except:
+            print("Config specified no output file")
+            writeToFile = False
         else:
-            print("!!! Invalid URL. Please try again. !!!")
-            if instructure_domain.find("http://") == -1 and instructure_domain.find("https://") == -1:
-                print("Remember to include \"http://\" or \"https://\"")
+            writeToFile = True
+        print("Making a request to the url to verify it exists...")
 
-    authtoken = input("Enter your authentication token. You can get this by going to:\n"
-                      "  1) Account\n"
-                      "  2) Settings\n"    
-                      "  3) + New Access Token\n"
-                      "\n!!! WARNING !!!: This token gives **FULL ACCESS** to your canvas account"
-                      "so be careful where you put this!\nTOKEN: ")
+        try:
+            testRequest = requests.get(instructure_domain)
+        except:
+            print("Invalid URL. Exiting")
+            exit(1)
+    if not configFileExists:
+        print("Try using arguments instead! python3 " + sys.argv[0] + " -h for more info.")
+        acceptz = input("Would you like to store your canvas URL, token, and output file settings in a config file? "
+                        "\nNote that while this may make it easier to run these scripts, random users may be able to "
+                        "get "
+                        "your token from the config files and use it! [Y/n]: ")
 
-    while authtoken == "" or len(authtoken) < 69:
-        authtoken = input("Token is too short. Try again: ")
+        if acceptz[0].lower() == 'y':
+            print("Saving entered data in config file")
+            createConfig = True
+            configCreator = ConfigParser()
+            configCreator['canvas_auth'] = {}
 
-    print(LINE*2)
-    print("Canvas URL: " + instructure_domain + "/api/v1")
-    print("Authtoken: " + authtoken)
-    accept = input("\nProceed? [Y/n]: ")
-    if accept != "" and accept[0].lower() == 'y':
-        print("Continuing")
-    else:
-        print("Canceled")
-        exit(1)
+        else:
+            print("Not saving data in config file")
+            createConfig = False
+        while True:
+            instructure_domain = input(
+                "\nPlease type the base URL for your canvas instance:\nURL: ")
 
-    outputsave = input("Save output to output.txt?\nThis will override any data already in this file! [Y/n]: ")
-    if outputsave != "" and outputsave[0].lower() == 'y':
-        print("Saving file to output.txt")
-        writeToFile = True
-        arg3 = "output.txt"
-    else:
-        print("Not saving output as file.")
-        writeToFile = False
+            valURL = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
+            if valURL.match(instructure_domain):
+                print("Making a request to the url to verify it exists...")
+                try:
+                    requests.get(instructure_domain)
+                except:
+                    print("Invalid URL. Please try again.")
+                else:
+                    if createConfig:
+                        configCreator['canvas_auth']['instance'] = instructure_domain
+                        print("Will save " + instructure_domain + " to config")
+                    break
+            else:
+                print("Invalid URL. Please try again.")
+                if instructure_domain.find("http://") == -1 and instructure_domain.find("https://") == -1:
+                    print("Remember to include \"http://\" or \"https://\"")
+
+        authtoken = input("Enter your authentication token. You can get this by going to:\n"
+                          "  1) Account\n"
+                          "  2) Settings\n"
+                          "  3) + New Access Token\n"
+                          "\nWARNING: Giving out this token to anyone will give them **FULL ACCESS** to your canvas "
+                          "account\nTOKEN: ")
+
+        while authtoken == "" or len(authtoken) < 68:
+            authtoken = input("Token is too short. Try again: ")
+        if createConfig:
+            configCreator['canvas_auth']['access_token'] = authtoken
+            print("Will save " + authtoken + " to config")
+
+        print(LINE * 2)
+        print("Canvas URL: " + instructure_domain + "/api/v1")
+        print("Authtoken: " + authtoken)
+        accept = input("\nProceed? [Y/n]: ")
+        if accept != "" and accept[0].lower() == 'y':
+            print("Continuing")
+        else:
+            print("Canceled")
+            exit(1)
+
+        outputsave = input("Save output to output.txt?\nThis will override any data already in this file! [Y/n]: ")
+        if outputsave != "" and outputsave[0].lower() == 'y':
+            print("Saving file to output.txt")
+            writeToFile = True
+            arg3 = "output.txt"
+            if createConfig:
+                configCreator['canvas_auth']['output_file'] = "output.txt"
+                print("Will enable output export in config")
+        else:
+            print("Not saving output as file.")
+            writeToFile = False
+            if createConfig:
+                print("Will disable output export in config")
 
 r = requests.get(instructure_domain + "/api/v1/users/self/courses?include[]=total_scores&include["
                                       "]=current_grading_period_scores&enrollment_type=student&include["
@@ -88,8 +141,8 @@ if courses == """b'{"errors":[{"message":"Invalid access token."}]}'""":
     print("Invalid access token. Exiting")
     exit(1)
 if r.status_code != 200:
-    print("Unknown response. Debug info is below:\nStatus_code: " 
-        + r.status_code + "\nResponse: " + courses)
+    print("Unknown response. Debug info is below:\nStatus_code: "
+          + r.status_code + "\nResponse: " + courses)
     exit(1)
 
 loc = [mfindID.start() for mfindID in list(re.finditer('\"id\"\:', courses))]
@@ -101,9 +154,14 @@ getPercentGrade = re.compile('\"computed_current_score\":[0-9.]+')
 
 if writeToFile: exportFile = open(arg3, 'w')
 
+if createConfig:
+    with open('config.ini', 'w') as configfile:
+        configCreator.write(configfile)
+    print("Config files saved.")
+
 for j in range(len(loc)):
     a = loc[j]
-    b = loc[j+1] if j != (len(loc) - 1) else len(courses)
+    b = loc[j + 1] if j != (len(loc) - 1) else len(courses)
 
     print(LINE)
     if writeToFile: exportFile.write(LINE + "\n")
@@ -111,7 +169,7 @@ for j in range(len(loc)):
     substr = courses[int(a):b]
     courseIDreg = getID.search(substr)
     courseID = substr[courseIDreg.start(0):courseIDreg.end(0)]
-    
+
     print("Course ID: " + courseID)
     if writeToFile:
         exportFile.write("Course ID: " + courseID + "\n")
