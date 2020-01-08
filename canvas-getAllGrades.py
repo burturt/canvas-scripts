ef66da2fbd63ad1c9e7f8436f6b5f62376eb0a66
burturt <31748545+burturt@users.noreply.github.com>
2020-01-07 21:14:07 -0800

    Try accepting pull request by first reverting

--- canvas-getAllGrades.py
+++ canvas-getAllGrades.py
@@ -1,139 +1,86 @@
-#!/bin/python3
-# canvas-getAllGrades.py
-# Enjoy!
-
 import re
 import requests
 import sys
-from configparser import ConfigParser
 
-LINE = "---------------------------"
-configFileExists = False
+print("This script will use a canvas API token to get a list of courses with grades and parse the output into "
+      "human-readable text.")
 
-if len(sys.argv) != 1 and len(sys.argv) != 3 and len(sys.argv) != 4:
-    print("Invalid # of arguments")
-    print("Usage: python3 <script path> <CANVAS_URL> <CANVAS_API_TOKEN> [FILENAME_TO_SAVE_TO.txt]")
-    print("OR\npython3 <script path>")
+loop = True
+loop2 = True
+loop3 = True
+writeToFile = False
+
+if (len(sys.argv) != 1) & (len(sys.argv) != 3) & (len(sys.argv) != 4):
+    print("Usage: python3 " + sys.argv[0] + "CANVAS_URL CANVAS_API_TOKEN (optional)FILENAME_TO_SAVE_TO.txt)\nThe "
+                                            "canvas url should be of format https://example.com, filename export is "
+                                            "optional.")
     exit(1)
-else:
-    print("\n" + LINE * 2)
-    print("Hello! Welcome to getAllGrades.py!\n")
-    print(
-        "This script utilizes your canvas API token to print a \nlist of your courses and grades in a human-readable "
-        "format.")
-    print(LINE * 2)
-
-if (len(sys.argv) == 3) or (len(sys.argv) == 4):
+elif (len(sys.argv) == 3) | (len(sys.argv) == 4):
     authtoken = sys.argv[2]
     instructure_domain = sys.argv[1]
-    createConfig = False
+    loop = False
+    loop2 = False
+    loop3 = False
     if (len(sys.argv) == 4):
         arg3 = sys.argv[3]
         writeToFile = True
-else:
-    try:
-        config = ConfigParser()
-        config.read('config.ini')
-        authtoken = config.get('canvas_auth', 'access_token')
-        instructure_domain = config.get('canvas_auth', 'instance')
-    except:
-        print("No valid config file found")
-    else:
-        createConfig = False
-        configFileExists = True
-        print("Valid config file (config.ini) found, using to populate options")
-        try:
-            arg3 = config.get('canvas_auth', 'output_file')
-        except:
-            print("Config specified no output file")
-            writeToFile = False
-        else:
-            writeToFile = True
+
+elif (len(sys.argv) == 1):
+    print("Try using arguments instead! python3 " + sys.argv[0] + " -h for more info.")
+
+while (loop):
+
+    instructure_domain = input("Please type the base URL for your canvas instance:\n")
+
+    valURL = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+    m = valURL.match(instructure_domain)
+    if m:
         print("Making a request to the url to verify it exists...")
 
         try:
             testRequest = requests.get(instructure_domain)
         except:
-            print("Invalid URL. Exiting")
-            exit(1)
-    if not configFileExists:
-        print("Try using arguments instead! python3 " + sys.argv[0] + " -h for more info.")
-        acceptz = input("Would you like to store your canvas URL, token, and output file settings in a config file? "
-                        "\nNote that while this may make it easier to run these scripts, random users may be able to "
-                        "get "
-                        "your token from the config files and use it! [Y/n]: ")
-
-        if acceptz[0].lower() == 'y':
-            print("Saving entered data in config file")
-            createConfig = True
-            configCreator = ConfigParser()
-            configCreator['canvas_auth'] = {}
-
+            print("Invalid URL. Please try again.")
         else:
-            print("Not saving data in config file")
-            createConfig = False
-        while True:
-            instructure_domain = input(
-                "\nPlease type the base URL for your canvas instance:\nURL: ")
+            loop = False
+    else:
+        print('Invalid URL. Please try again')
+        if (instructure_domain.find("http://") == -1 & instructure_domain.find("https://") == -1):
+            print('Remember to include http:// or https:// at the beginning of the URL!')
 
-            valURL = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
-            if valURL.match(instructure_domain):
-                print("Making a request to the url to verify it exists...")
-                try:
-                    requests.get(instructure_domain)
-                except:
-                    print("Invalid URL. Please try again.")
-                else:
-                    if createConfig:
-                        configCreator['canvas_auth']['instance'] = instructure_domain
-                        print("Will save " + instructure_domain + " to config")
-                    break
-            else:
-                print("Invalid URL. Please try again.")
-                if instructure_domain.find("http://") == -1 and instructure_domain.find("https://") == -1:
-                    print("Remember to include \"http://\" or \"https://\"")
+while (loop2):
+    authtoken = input("Please paste your authentication token. You can get this by going to account --> "
+                              "settings and creating a new access token.\nWord of warning: giving out this token to "
+                              "anyone will give them **FULL ACCESS** to your canvas account, so be careful where you "
+                              "put "
+                              "this!\n")
+    if authtoken != '':
+        loop2 = False
+    else:
+        print("No token provided.")
 
-        authtoken = input("Enter your authentication token. You can get this by going to:\n"
-                          "  1) Account\n"
-                          "  2) Settings\n"
-                          "  3) + New Access Token\n"
-                          "\nWARNING: Giving out this token to anyone will give them **FULL ACCESS** to your canvas "
-                          "account\nTOKEN: ")
+authhead = {"Authorization": "Bearer " + authtoken}
 
-        while authtoken == "" or len(authtoken) < 68:
-            authtoken = input("Token is too short. Try again: ")
-        if createConfig:
-            configCreator['canvas_auth']['access_token'] = authtoken
-            print("Will save " + authtoken + " to config")
+if loop3:
 
-        print(LINE * 2)
-        print("Canvas URL: " + instructure_domain + "/api/v1")
-        print("Authtoken: " + authtoken)
-        accept = input("\nProceed? [Y/n]: ")
-        if accept != "" and accept[0].lower() == 'y':
-            print("Continuing")
-        else:
-            print("Canceled")
-            exit(1)
+    accept = input(
+        "Making a request to the url " +
+        instructure_domain + "/api/v1 using the authentication token " +
+        authtoken + ". Proceed? [Y/n]: ")
 
-        outputsave = input("Save output to output.txt?\nThis will override any data already in this file! [Y/n]: ")
-        if outputsave != "" and outputsave[0].lower() == 'y':
-            print("Saving file to output.txt")
-            writeToFile = True
-            arg3 = "output.txt"
-            if createConfig:
-                configCreator['canvas_auth']['output_file'] = "output.txt"
-                print("Will enable output export in config")
-        else:
-            print("Not saving output as file.")
-            writeToFile = False
-            if createConfig:
-                print("Will disable output export in config")
+    if accept[0].lower() == 'y':
+        print("Continuing")
+
+    else:
+        print("Canceled")
+        exit(1)
+
 
 r = requests.get(instructure_domain + "/api/v1/users/self/courses?include[]=total_scores&include["
                                       "]=current_grading_period_scores&enrollment_type=student&include["
                                       "]=concluded&per_page=1000",
-                 headers={"Authorization": "Bearer " + authtoken})
+                 headers=authhead)
 
 courses = str(r.content)
 
@@ -141,9 +88,21 @@ if courses == """b'{"errors":[{"message":"Invalid access token."}]}'""":
     print("Invalid access token. Exiting")
     exit(1)
 if r.status_code != 200:
-    print("Unknown response. Debug info is below:\nStatus_code: "
-          + r.status_code + "\nResponse: " + courses)
+    print("Unknown response. Debug info is below:")
+    print("Status_code: " + r.status_code + "\nResponse: " + courses)
     exit(1)
+
+if loop3:
+
+    outputsave = input("Save output to output.txt? This will override any data already in this file! [Y/n]\n")
+
+    if outputsave[0].lower() == 'y':
+        print("Saving file to output.txt")
+        writeToFile = True
+        arg3 = "output.txt"
+
+    else:
+        print("Not saving output as file.")
 
 loc = [mfindID.start() for mfindID in list(re.finditer('\"id\"\:', courses))]
 
@@ -152,34 +111,35 @@ getID = re.compile('[0-9]+')
 getLetterGrade = re.compile('\"computed_current_grade\":\"[A-Za-z+-]+\"')
 getPercentGrade = re.compile('\"computed_current_score\":[0-9.]+')
 
-if writeToFile: exportFile = open(arg3, 'w')
-
-if createConfig:
-    with open('config.ini', 'w') as configfile:
-        configCreator.write(configfile)
-    print("Config files saved.")
+if writeToFile:
+    exportFile = open(arg3, 'w')
 
 for j in range(len(loc)):
     a = loc[j]
-    b = loc[j + 1] if j != (len(loc) - 1) else len(courses)
-
-    print(LINE)
-    if writeToFile: exportFile.write(LINE + "\n")
+    if j != (len(loc) - 1):
+        b = loc[j+1]
+    else:
+        b = len(courses)
+    print("---------------------------")
+    if writeToFile:
+        exportFile.write("---------------------------\n")
 
     substr = courses[int(a):b]
+
     courseIDreg = getID.search(substr)
     courseID = substr[courseIDreg.start(0):courseIDreg.end(0)]
-
-    print("Course ID: " + courseID)
     if writeToFile:
         exportFile.write("Course ID: " + courseID + "\n")
+    print("Course ID: " + courseID)
 
     if "restricted" in substr[courseIDreg.end(0):b]:
         print("Course not available yet")
         if writeToFile:
-            exportFile.write("Course not available yet\n")
+            exportFile.write("Course not available yet" + "\n")
     else:
+
         try:
+
             mGetName = getName.search(substr)
             substr2 = courses[mGetName.start(0) + a + 8:mGetName.end(0) + a - 1]
             print("Name: " + substr2)
@@ -188,9 +148,10 @@ for j in range(len(loc)):
         except:
             print("No Course Name found")
             if writeToFile:
-                exportFile.write("No Course Name found\n")
+                exportFile.write("No Course Name found" + "\n")
 
         try:
+
             mGetLetterGrade = getLetterGrade.search(substr)
             substr2 = courses[mGetLetterGrade.start(0) + a + 26:mGetLetterGrade.end(0) + a - 1]
             print("Letter Grade: " + substr2)
@@ -199,22 +160,22 @@ for j in range(len(loc)):
         except:
             print("No Letter Grade found")
             if writeToFile:
-                exportFile.write("No Letter Grade found\n")
-
+                exportFile.write("No Letter Grade found" + "\n")
         try:
+
             mGetPercentGrade = getPercentGrade.search(substr)
             substr2 = courses[mGetPercentGrade.start(0) + a + 25:mGetPercentGrade.end(0) + a]
             print("Percent Grade: " + substr2 + "%")
             if writeToFile:
-                exportFile.write("Percent Grade: " + substr2 + "%\n")
+                exportFile.write("Percent Grade: " + substr2 + "%" + "\n")
         except:
             print("No Percent Grade found")
             if writeToFile:
-                exportFile.write("No Percent Grade found\n")
+                exportFile.write("No Percent Grade found" + "\n")
 
-print(LINE)
+print("---------------------------")
 if writeToFile:
-    exportFile.write(LINE + "\n")
+    exportFile.write("---------------------------" + "\n")
     exportFile.close()
     print("Successfully exported to file " + arg3)
 print("Done")
