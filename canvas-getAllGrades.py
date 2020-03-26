bb0c158b654323ce23dae8bc5eeefb5210a1d2ca
burturt <31748545+burturt@users.noreply.github.com>
2020-03-26 16:18:09 -0700

    Update canvas-getAllGrades.py

--- canvas-getAllGrades.py
+++ canvas-getAllGrades.py
@@ -1,6 +1,21 @@
 #!/bin/python3
 # canvas-getAllGrades.py
 # Enjoy!
+#    Canvas-Get Grades
+#    Copyright (C) 2020  burturt, B0kCh01
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import re
 import requests
@@ -9,6 +24,16 @@ from configparser import ConfigParser
 
 LINE = "---------------------------"
 configFileExists = False
+
+
+print("Canvas-getAllGrades  Copyright (C) 2020  burturt, B0kCh01")
+print("This program comes with ABSOLUTELY NO WARRANTY; for details see https://git.io/Jv9Hg.")
+print("This is free software, and you are welcome to redistribute it")
+print("under certain conditions; see https://git.io/Jv9Hg details.")
+print("Should the above links not work, please refer to the GNU General Public License as published by the Free "
+      "Software Foundation, either version 3 of the License, or any later version. A copy of that license should have "
+      "been included with this program.") 
+
 
 if len(sys.argv) != 1 and len(sys.argv) != 3 and len(sys.argv) != 4:
     print("Invalid # of arguments")
